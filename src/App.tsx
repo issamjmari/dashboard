5e9eac2fe18dd5498555dce5ef9648274549aa9e
Issam Jmari <ijmari@e1r8p5.1337.ma>
2024-02-04 18:52:55 +0100

    starting the dashboard project

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Header from "./components/Menu/menu";
+import Footer from "./components/Footer/footer";
+import Navbar from "./components/navbar/navbar";
+import Menu from "./components/Menu/menu";
+import Users from "./pages/Users/Users";
+import Products from "./pages/Products/Products";
+import Home from "./pages/Home/Home";
+import './styles/global.scss'
+
+function App() {
+  const Layout = () => {
+      return (
+        <div className="main">
+            <Navbar />
+            <div className="container">
+                <div className="menuContainer">
+                    <Menu />
+                </div>
+                <div className="contentContainer">
+                  <Outlet />
+                </div>
+            </div>
+            <Footer />
+        </div>
+      ) 
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/users",
+          element: <Users />
+        }, 
+        {
+          path: "/products",
+          element: <Products />
+        },
+        {
+          path: "/",
+          element: <Home />
+        }
+      ]
+    }
+    
+  ])
+  return <RouterProvider router={router}/>
+}
+
+export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import './navbar.scss'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+        <div className='logo'>
+            <img src='/logo.svg' alt="logo"/>
+            <span>lamadmin</span>
+        </div>
+        <div className='icons'>
+          <img src='/search.svg' alt="logo"/>
+          <img src='/app.svg' alt="logo"/>
+          <img src='/expand.svg' alt="logo"/>
+          <div className='notification'>
+            <img src='/notifications.svg' alt="logo"/>
+            <span>1</span>
+          </div>
+          <div className='user'>
+            <img
+              src='https://cdn.intra.42.fr/users/1312e60531fd16557839bc942ae10856/ijmari.jpg'
+              alt="user image"
+             />
+             <span>Issam</span>
+          </div>
+          <img src='/settings.svg' alt="settings"/>
+        </div>
+    </div>
+  )
+}
